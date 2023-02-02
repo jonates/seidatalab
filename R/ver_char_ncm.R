f925c228f5d1794f960a9d6454a47053a7c096e6
SamuPortela <61995333+SamuPortela@users.noreply.github.com>
2023-02-02 10:39:13 -0300

    modificação da abordagem lógica

--- R/ver_char_ncm.R
+++ R/ver_char_ncm.R
@@ -1,27 +1,14 @@
 #' Detectar os NCMs válidos
 #' 
-#' Recebe um vetor com o código da Nomenclatura Comum do Mercosul e retorna as observações adicionando uma coluna binária referente a validação, onde 0 = "Inválido" e 1 = "Válido".
+#' Recebe um vetor com o código da Nomenclatura Comum do Mercosul e retorna os valores únicos adicionando uma coluna binária referente a validação, onde 0 = "Inválido" e 1 = "Válido".
 #' 
 #' @param data vetor de códigos ncm.
 #' 
 #' @export
-ver_char_ncm <- function(data, var) {
-  if (sparklyr::sdf_schema(data)[[1]][2]$type == "StringType") {
-    data <- dplyr::mutate(
-      data,
-      VALID_NCM = dplyr::case_when(
-         nchar(glue::trim(data[,1])) == 8 ~ 1,
-         TRUE ~ as.character(data[,1])
-      )
-    )
-  } else {
-      data <- dplyr::mutate(
-        data,
-        VALID_NCM = dplyr::case_when(
-          nchar(as.character(as.integer(data$var))) == 8 ~ 1,
-          TRUE ~ as.character(data$var)
-        )
-      )
-    }
+ver_char_ncm <- function (data, var) {
+  data <- as.data.frame(dplyr::distinct(data[var]))
+  data <- mutate(data,
+                 VALID_NCM = dplyr::case_when(nchar(trimws(data[,1])) == 8 ~ 1,
+                                              TRUE ~ 0))
   return(data)
   }
